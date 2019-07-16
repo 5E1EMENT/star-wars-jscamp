f9c73c4328eba5228aebeef3b60b5f7361ae8f1c
daniilnikitas <daniil.nikitas@saritasa.com>
2019-07-16 12:51:33 +0600

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MatSidenavModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    LayoutModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

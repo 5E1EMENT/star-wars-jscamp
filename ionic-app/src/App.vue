b51a205b909869caf053b95755a94402a13f2186
daniilnikitas <daniil.nikitas@saritasa.com>
2019-08-21 10:22:14 +0600

    menu sidebar functional added

--- ionic-app/src/App.vue
+++ ionic-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <ion-app>
+      <Menu />
       <ion-vue-router />
     </ion-app>
     <ion-menu-controller></ion-menu-controller>
@@ -8,8 +9,11 @@
 </template>
 
 <script>
+import Menu from '@/components/TheMenu.vue'
+
 export default {
-  name: "App"
+  name: "App",
+  components: {Menu}
 };
 </script>
 
--- ionic-app/src/app/films/components/Films.vue
+++ ionic-app/src/app/films/components/Films.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ion-page">
+  <div class="ion-page" main>
     <ion-header>
       <ion-toolbar color="primary" class="toolbar-md-primary">
         <ion-title>Register</ion-title>
@@ -7,6 +7,7 @@
     </ion-header>
     <ion-content class="ion-padding">
       Films
+      <ion-button @click="openStart">Open Start Menu</ion-button>
     </ion-content>
   </div>
 </template>
